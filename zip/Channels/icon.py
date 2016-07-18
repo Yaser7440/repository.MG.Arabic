7597e4b1832735aae07cfca9848361bb1a951e48
MG <a_yaser@live.com>
2016-07-18 20:29:52 +0300

    TVicon

---
+++ zip/Channels/icon.py
@@ -0,0 +1,332 @@
+HARDCODED_STREAMS = [{
+                         'title': 'MBC',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/mbc_1.jpg',
+                         'path': 'mbc_1'},
+                     {
+                         'title': 'BeIn Sport 1',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/bein_sport_1.jpg',
+                         'path': 'bein_sport_1'},
+                     {
+                         'title': 'BeIn Sport 2',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/bein_sport_2.jpg',
+                         'path': 'bein_sport_2'},
+                     {
+                         'title': 'BeIn Sport 3',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/bein_sport_3.jpg',
+                         'path': 'bein_sport_3'},
+                     {
+                         'title': 'BeIn Sport 4',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/bein_sport_4.jpg',
+                         'path': 'bein_sport_4'},
+                     {
+                         'title': 'BeIn Sport 5',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/bein_sport_5.jpg',
+                         'path': 'bein_sport_5'},
+                     {
+                         'title': 'BeIn Sport 6',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/bein_sport_6.jpg',
+                         'path': 'bein_sport_6'},
+                     {
+                         'title': 'BeIn Sport 7',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/bein_sport_7.jpg',
+                         'path': 'bein_sport_7'},
+                     {
+                         'title': 'BeIn Sport 8',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/bein_sport_8.jpg',
+                         'path': 'bein_sport_8'},
+                     {
+                         'title': 'BeIn Sport 9',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/bein_sport_9.jpg',
+                         'path': 'bein_sport_9'},
+                     {
+                         'title': 'Abu Dhabi Al Oula',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/abu_dhabi.jpg',
+                         'path': 'abu_dhabi'},
+                     {
+                         'title': 'Abu Dhabi Sports',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/abu_dhabi_sports_1.jpg',
+                         'path': 'abu_dhabi_sports_1'},
+                     {
+                         'title': 'Al Jazeera',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/aljazeera.jpg',
+                         'path': 'aljazeera'},
+                     {
+                         'title': 'Aljadeed',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/aljadeed.jpg',
+                         'path': 'aljadeed'},
+                     {
+                         'title': 'OTV Lebanon',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/otv_lebanon.jpg',
+                         'path': 'otv_lebanon'},
+                     {
+                         'title': 'Al-Nahar',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/al_nahar.jpg',
+                         'path': 'al_nahar'},
+                     {
+                         'title': 'Al Hayat 1',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/alhayat_1.jpg',
+                         'path': 'alhayat_1'},
+                     {
+                         'title': 'Al Hayat Series',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/al_hayat_series.jpg',
+                         'path': 'al_hayat_series'},
+                     {
+                         'title': 'Al Hayat Cinema',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/al_hayat_cinema.jpg',
+                         'path': 'al_hayat_cinema'},
+                     {
+                         'title': 'ART Aflam 2',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/art_aflam_2.jpg',
+                         'path': 'art_aflam_2'},
+                     {
+                         'title': 'Al Jazeera Children',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/aljazeera_children.jpg',
+                         'path': 'aljazeera_children'},
+                     {
+                         'title': 'ART Aflam 1',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/art_aflam_1.jpg',
+                         'path': 'art_aflam_1'},
+                     {
+                         'title': 'ART Cinema',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/art_cinema.jpg',
+                         'path': 'art_cinema'},
+                     {
+                         'title': 'ART Hikayat 1',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/art_hikayet_1.jpg',
+                         'path': 'art_hikayet_1'},
+                     {
+                         'title': 'ART Hikayat 2',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/art_hikayet_2.jpg',
+                         'path': 'art_hikayet_2'},
+                     {
+                         'title': 'Tele Liban',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/tele_liban.jpg',
+                         'path': 'tele_liban'},
+                     {
+                         'title': 'Al Hayat 2',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/alhayat_2.jpg',
+                         'path': 'alhayat_2'},
+                     {
+                         'title': 'Alarabiya',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/alarabiya.jpg',
+                         'path': 'alarabiya'},
+                     {
+                         'title': 'Al Moutawassit',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/al_moutawasit.jpg',
+                         'path': 'al_mutawasit'},
+                     {
+                         'title': 'CBC',
+                         'thumbnail': 'http://www.teledunet.com/logo/CBC.jpg',
+                         'path': 'cbc'},
+                     {
+                         'title': 'CBC Extra',
+                         'thumbnail': 'http://www.teledunet.com/logo/CBC%20Extra.jpg',
+                         'path': 'cbc_extra'},
+                     {
+                         'title': 'CBC 2',
+                         'thumbnail': 'http://www.teledunet.com/logo/CBC%202.jpg',
+                         'path': 'cbc_2'},
+                     {
+                         'title': 'CBC Sofra',
+                         'thumbnail': 'http://www.teledunet.com/logo/CBC%20Sofra.jpg',
+                         'path': 'cbc_sofra'},
+                     {
+                         'title': 'Al Rafidain',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/al_rafidain.jpg',
+                         'path': 'al_rafidain'},
+                     {
+                         'title': 'NBN',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/nbn.jpg',
+                         'path': 'nbn'},
+                     {
+                         'title': 'Panorama Drama 2',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/panorama_drama_2.jpg',
+                         'path': 'panorama_drama_2'},
+                     {
+                         'title': 'Panorama Drama',
+                         'thumbnail': 'http://www.teledunet.com/logo/Panorama%20Drama.jpg',
+                         'path': 'panorama_drama'},
+                     {
+                         'title': 'Panorama Comedy',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/panorama_comedy.jpg',
+                         'path': 'panorama_comedy'},
+                     {
+                         'title': 'Sada El Balad +2',
+                         'thumbnail': 'http://www.teledunet.com/logo/Sada%20Albalad.jpg',
+                         'path': 'sada_albalad'},
+                     {
+                         'title': 'Libya AlAhrar',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/libya_alahrar.jpg',
+                         'path': 'libya_alahrar'},
+                     {
+                         'title': 'Libya Alwatania 1',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/libya_alwatania_1.jpg',
+                         'path': 'libya_alwatania_1'},
+                     {
+                         'title': 'Dzair TV',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/dzair_tv.jpg',
+                         'path': 'dzair_tv'},
+                     {
+                         'title': 'Sada El Balad',
+                         'thumbnail': 'http://www.teledunet.com/logo/Sada%20Albalad.jpg',
+                         'path': 'sada_el_balad'},
+                     {
+                         'title': 'MBC 3',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/mbc_3.jpg',
+                         'path': 'mbc_3'},
+                     {
+                         'title': 'MBC Drama',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/mbc_drama.jpg',
+                         'path': 'mbc_drama'},
+                     {
+                         'title': 'MBC Masr',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/mbc_masr.jpg',
+                         'path': 'mbc_masr'},
+                     {
+                         'title': 'On Tv',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/on_tv.jpg',
+                         'path': 'on_tv'},
+                     {
+                         'title': 'Oromia TV',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/oromia_tv.jpg',
+                         'path': 'oromia_tv'},
+                     {
+                         'title': 'Tchad',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/tchad.jpg',
+                         'path': 'tchad'},
+                     {
+                         'title': 'Wanasah',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/wanasah.jpg',
+                         'path': 'wanasah'},
+                     {
+                         'title': 'Roya',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/roya.jpg',
+                         'path': 'roya'},
+                     {
+                         'title': 'Kalsan TV',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/kalsan_tv.jpg',
+                         'path': 'kalsan_tv'},
+                     {
+                         'title': 'El Djazairia',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/el_djazairia.jpg',
+                         'path': 'el_djazairia'},
+                     {
+                         'title': 'Al Ekhbariyah Al Syria',
+                         'thumbnail': 'http://www.teledunet.com/logo/Al%20Ikhbaria.jpg',
+                         'path': 'al_ikhbaria'},
+                     {
+                         'title': 'MTV LB',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/mtv.jpg',
+                         'path': 'mtv'},
+                     {
+                         'title': 'Djibouti TV',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/djibouti.jpg',
+                         'path': 'djibouti'},
+                     {
+                         'title': 'Libya Alriadya 1',
+                         'thumbnail': 'http://en.libyamediawiki.com/images/4/40/Lrt_ly_sport.jpeg',
+                         'path': 'libya_alriadya_1'},
+                     {
+                         'title': 'Al Kahera Wal Nas',
+                         'thumbnail': 'http://www.alkaherawalnas.com/images/logo-01.png',
+                         'path': 'al_kahera_wal_nas'},
+                     {
+                         'title': 'Al Kahera Wal Nas 2',
+                         'thumbnail': 'http://www.alkaherawalnas.com/images/logo-01.png',
+                         'path': 'al_kahera_wal_nas_2'},
+                     {
+                         'title': 'Al Oula',
+                         'thumbnail': 'http://www.satexpat.com/img/cha/eg/ertu_1_95x68.gif',
+                         'path': 'al_oula'},
+                     {
+                         'title': 'Alanwar',
+                         'thumbnail': 'http://www.lyngsat-logo.com/logo/tv/aa/al_anwar.jpg',
+                         'path': 'alanwar'},
+                     {
+                         'title': 'Aldafrah',
+                         'thumbnail': 'http://www.lyngsat-logo.com/logo/tv/aa/al_dafrah.png',
+                         'path': 'aldafrah'},
+                     {
+                         'title': 'Alhewar',
+                         'thumbnail': 'http://www.lyngsat-logo.com/hires/aa/al_hiwar.png',
+                         'path': 'alhewar'},
+                     {
+                         'title': 'Aliraqiya Sport',
+                         'thumbnail': 'http://upload.wikimedia.org/wikipedia/en/9/98/Al_Iraqiya_logo.png',
+                         'path': 'aliraqiya_sport'},
+                     {
+                         'title': 'Bein News',
+                         'thumbnail': 'http://upload.wikimedia.org/wikipedia/commons/2/20/Bein_sport_logo.png',
+                         'path': 'bein_news'},
+		     {
+                         'title': 'Dalel',
+                         'thumbnail': 'http://www.invision.com.sa/en/sites/default/files/imagecache/216x216/channels/2011/10/11/1154.jpg',
+                         'path': 'dalel'},
+                     {
+                         'title': 'Es TV',
+                         'thumbnail': 'http://www.tv-logo.com/pt-data/uploads/images/logo/ethiopia_somali_tv.jpg',
+                         'path': 'estv'},
+                     {
+                         'title': 'ETV Ethiopia',
+                         'thumbnail': 'http://1.bp.blogspot.com/-m4XUFK-_cZI/UWT0ntZBbmI/AAAAAAAAAx0/MleTQ1XcI6k/s400/Ethiopian%2BTV%2BETV.jpg',
+                         'path': 'etv_ethiopia'},
+                     {
+                         'title': 'Euro News',
+                         'thumbnail': 'http://img3.wikia.nocookie.net/__cb20091205135554/logopedia/images/9/93/Euronews_logo.svg',
+                         'path': 'euro_news'},
+                     {
+                         'title': 'Future',
+                         'thumbnail': 'http://www.arabe-media.com/tv/img84.jpg',
+                         'path': 'future'},
+                     {
+                         'title': 'Knn News',
+                         'thumbnail': 'http://www.freeetv.com/images/reydddd/KNN.jpg',
+                         'path': 'knn_news'},
+                     {
+                         'title': 'Noonkids',
+                         'thumbnail': 'http://www.lyngsat-logo.com/hires/nn/noon_kids_tv_jo.png',
+                         'path': 'noonkids'},
+                     {
+                         'title': 'Ishtar',
+                         'thumbnail': 'http://www.lyngsat-logo.com/hires/ii/ishtar_tv.png',
+                         'path': 'ishtar'},
+                     {
+                         'title': 'Noor Alsham',
+                         'thumbnail': 'http://upload.wikimedia.org/wikipedia/en/b/bf/Noor_Al-Sham_Logo.png',
+                         'path': 'noor_alsham'},
+                     {
+                         'title': 'Orient News',
+                         'thumbnail': 'http://radioteve.com/tele/arabes/arabemirates/logo/orientnews.jpg',
+                         'path': 'orient_news'},
+                     {
+                         'title': 'Oscar Drama',
+                         'thumbnail': 'http://www.lyngsat-logo.com/hires/oo/oscar_drama.png',
+                         'path': 'oscar_drama'},
+                     {
+                         'title': 'Sama Syria',
+                         'thumbnail': 'http://upload.wikimedia.org/wikipedia/en/7/75/Sama_TV_Logo.png',
+                         'path': 'sama_syria'},
+                     {
+                         'title': 'Skynews',
+                         'thumbnail': 'http://img2.wikia.nocookie.net/__cb20100419120542/logopedia/images/c/c4/Sky_News_2007.png',
+                         'path': 'skynews'},
+                     {
+                         'title': 'Star Cinema 1',
+                         'thumbnail': 'http://www.magictvbox.com/wp-content/uploads/2013/05/star-cinema-1-channel1.jpg',
+                         'path': 'star_cinema'},
+                     {
+                         'title': 'Star Cinema 2',
+                         'thumbnail': 'http://www.magictvbox.com/wp-content/uploads/2013/05/star-cinema-channel.jpg',
+                         'path': 'star_cinema_2'},
+                     {
+                         'title': 'Sudan',
+                         'thumbnail': 'http://www.lyngsat-logo.com/hires/ss/sudan_tv.png',
+                         'path': 'sudan'},
+                     {
+                         'title': 'Telvza TV',
+                         'thumbnail': 'http://www.madwatch.net/wp-content/uploads/2013/12/LOGO-telvza.jpg',
+                         'path': 'telvza_tv'},
+                     {
+                         'title': 'Cartoon Network',
+                         'thumbnail': 'http://www.teledunet.com//player/icones/cartoon_network.jpg',
+                         'path': 'cartoon_network'}]
