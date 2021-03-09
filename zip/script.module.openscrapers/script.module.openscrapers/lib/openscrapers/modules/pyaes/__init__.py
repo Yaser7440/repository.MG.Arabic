9be509a16868b2edeb2f9ea23cc6e2b7f7aea16e
MG <a_yaser@live.com>
2021-03-09 13:02:33 +0300

    test

---
+++ zip/script.module.openscrapers/script.module.openscrapers/lib/openscrapers/modules/pyaes/__init__.py
@@ -0,0 +1,15 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Richard Moore
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribpcr/NIT Li,twar/or selo deuseiesopy
+# in the Sof,twarhts
+p#
+# to any pshts
+whom
+# in the Softirighfurn puerhts
+do thibpcrjecthts
+# infollownclucondiimits:Moore
