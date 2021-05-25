94ebcf003a5e78b4d75244b058cef26c6abf92a7
MG <a_yaser@live.com>
2021-05-25 16:05:28 +0300

    new

--- zip/script.module.exoscrapers/lib/exoscrapers/sources_exoscrapers/ar/faselhd.py
+++ zip/script.module.exoscrapers/lib/exoscrapers/sources_exoscrapers/ar/faselhd.py
@@ -2,40 +2,41 @@
 # -Cleaned and Checked on 07-23-2019 by MGArabic in Scrubs.
 # Has shows but is shitty and limited.
 
-import urllib, traceback, re
-from exoscrapers.modules import client ,log_utils ,cleantitle ,source_utils
+import urllib, re, traceback
+from exoscrapers.modules import client, log_utils, cleantitle, source_utils
 
-class source:	
-	def __init__(self):
-		self.priority = 1
-		self.language = ['en']
-		self.domains = ['faselhd.pro']
-		self.base_link = 'https://www.faselhd.pro'
-		self.episode_link = '/episodes/مسلسل-%s-الموسم-%s-الحلقه-%s'
-		self.episode2_link = '/episodes/مسلسل-%s-الموسم-%s-الحلقة-%s'
-		self.season_link = '/seasons/مسلسل-%s'
+class source:
+    def __init__(self):
+        self.priority = 1
+        self.language = ['en']
+        self.domains = ['www.faselhd.pro']
+        self.base_link = 'https://www.faselhd.pro'
+		
+		
 
-	def movie(self, imdb, title, localtitle, aliases, year):
-		try:
-			mtitle = cleantitle.get_url(title).replace('-','+').replace(':','').replace('&','+').replace("'",'+')
-			mtitle = cleantitle.geturl(mtitle)
-			url = self.base_link + '/search/%s+%s/feed/rss2/' % (mtitle, year)
-			r = client.request(url)
-			items = re.compile('<item>(.+?)</item>', re.DOTALL).findall(r)
-			for item in items:
-				match = re.compile('<title>(.+?)</title>.+?<link>(.+?)</link>', re.DOTALL).findall(item)
-				for row_title, row_url in match:
-					if cleantitle.get(title) in cleantitle.get(row_title):
-						if year in str(row_title):
-							return row_url
-			return
-		except Exception:
+    def movie(self, imdb, title, localtitle, aliases, year):
+        try:
+            mtitle = cleantitle.get_url(title).replace('-','+').replace(':','').replace('&','+').replace("'",'+')
+            mtitle = cleantitle.geturl(mtitle)
+            url = self.base_link + '/search/%s+%s/feed/rss2/' % (mtitle, year)
+            r = client.request(url)
+            items = re.compile('<item>(.+?)</item>', re.DOTALL).findall(r)
+            for item in items:
+                match = re.compile('<title>(.+?)</title>.+?<link>(.+?)</link>', re.DOTALL).findall(item)
+                for row_title, row_url in match:
+                    if cleantitle.get(title) in cleantitle.get(row_title):
+                        if year in str(row_title):
+                            return row_url
+                        
+            return url
+        except Exception:
 			failure = traceback.format_exc()
 			log_utils.log('---FASELHD Testing - Exception: \n' + str(failure))
 			return
 
-	def tvshow(self, imdb, tvdb, tvshowtitle, localtvshowtitle, aliases, year):
-		try:
+
+    def tvshow(self, imdb, tvdb, tvshowtitle, localtvshowtitle, aliases, year):
+        try:
 			mtitle = cleantitle.get_url(tvshowtitle).replace('-','+').replace(':','').replace('&','+').replace("'",'+')
 			mtitle = cleantitle.geturl(mtitle)
 			url = self.base_link + '/search/%s/feed/rss2/' % tvshowtitle
@@ -45,19 +46,19 @@ class source:
 				match = re.compile('<title>(.+?)</title>.+?<link>(.+?)</link>', re.DOTALL).findall(item)
 				for row_title, row_url in match:
 					if cleantitle.get(tvshowtitle) in cleantitle.get(row_title):
-						#log_utils.log('tvshowfaseldom = %s' % cleantitle.get(tvshowtitle), log_utils.LOGDEBUG)
 						return row_url
 			return
-		except Exception:
+        except Exception:
 			failure = traceback.format_exc()
 			log_utils.log('---FASELHD Testing - Exception: \n' + str(failure))
 			return
 
 
-	def episode(self, url, imdb, tvdb, title, premiered, season, episode):
-		try:
+    def episode(self, url, imdb, tvdb, title, premiered, season, episode):
+        try:
 			if not url:
 				return
+			log_utils.log('url = %s' % url, log_utils.LOGDEBUG)
 			r = client.request(url)
 			seasons = re.compile('id="seasonList">(.+?)<script', re.DOTALL).findall(r)
 			for se in seasons:
@@ -74,81 +75,56 @@ class source:
 									return row_url
 
 			return
-		except Exception:
+        except Exception:
 			failure = traceback.format_exc()
 			log_utils.log('---FASELHD Testing - Exception: \n' + str(failure))
 			return
 
 
-	def sources(self, url, hostDict, hostprDict):
-		try:
-			sources = []
-			if url is None:
-				return sources
-			hostDict = hostDict + hostprDict
-			headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'}
-			r = client.request(url, headers=headers, XHR=True, timeout='10')
-			results = re.compile('player_iframe.+?=\s(?:\"|\')(.+?)(?:&img=|\')', re.DOTALL).findall(r)
-			for link in results:
-				link = "https:" + link if not link.startswith('http') else link
-				link = client.replaceHTMLCodes(link.replace('%3A', ':').replace('%2F', '/'))
-				link = link.replace('https://www.faselhd.life/player/embed.php?url=','').replace('nv2=true&uid=0&','').replace('https://www.faselhd.live/player/embed.php?url=','')
-				#log_utils.log('link = %s' % link, log_utils.LOGDEBUG)
-				if 'faselhd' in link:
-					d = client.request(link, headers=headers)
-					videos = re.compile('(?:file:)\s*(?:\"|\')(.+?)(?:\"|\')', re.DOTALL).findall(d)
-					for video in videos:
-						video += '|Referer=%s&User-Agent=%s' % (urllib.quote(client.agent()), link)
-						sources.append({'source': 'faseldom', 'quality': '1080p', 'language': 'ar',
-										'url': video, 'direct': True, 'debridonly': False})
-						
-				else:
-					valid, host = source_utils.is_host_valid(link, hostDict)
-					if valid:
-						if host in str(sources): 
-							continue
-						sources.append({'source': host, 'quality': '1080p', 'language': 'ar',
-										'url': link, 'direct': False, 'debridonly': False})
+    def sources(self, url, hostDict, hostprDict):
+        try:
+            sources = []
+            if url is None:
+                return sources
+            hostDict = hostDict + hostprDict
+            log_utils.log('url = %s' % url, log_utils.LOGDEBUG)
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'}
+            r = client.request(url)
+            results = re.compile('player_iframe.+?=\s(?:\"|\')(.+?)(?:&img=|\')', re.DOTALL).findall(r)
+            for url in results:
+				url = client.replaceHTMLCodes(url.replace('%2F','/').replace('%3A',':').replace('%3F','?').replace('\r','').replace('\n',''))
+				url = url.replace('https://www.faselhd.life/player/embed.php?url=','').replace('nv2=true&uid=0&','').replace('https://www.faselhd.live/player/embed.php?url=','')
+				url = url.replace('faselhd.life','faselhd.pro')
+				url = "https:" + url if not url.startswith('http') else url
+				if 'faselhd' in url:
+						try:
+							result = client.request(url, headers=headers)
+							match = re.compile('(?:file:)\s*(?:\"|\')(.+?)(?:\"|\')', re.DOTALL).findall(result)
+							for link in match:
+								link = link.replace('"','')
+								link = "https:" + link if not link.startswith('http') else link
+								# link += '|Referer=%s&User-Agent=%s' % (urllib.quote(client.agent()), url)
+								log_utils.log('link = %s' % link, log_utils.LOGDEBUG)
+								sources.append({'source': 'faseldom', 'quality': '1080p', 'language': 'ar', 'url': link,
+												'direct': True, 'debridonly': False})
+						except Exception:
+							pass
 
-			return sources
-		except Exception:
+				else:					
+
+					
+						valid, host = source_utils.is_host_valid(url, hostDict)
+						if valid:
+							if host in str(sources):
+								continue
+							quality, info = source_utils.get_release_quality(url, url)			
+							sources.append({'source': host, 'quality': quality, 'language': 'ar', 'info': '', 
+										'url': url, 'direct': False, 'debridonly': False})
+            return sources
+        except Exception:
 			failure = traceback.format_exc()
 			log_utils.log('---FASELHD Testing - Exception: \n' + str(failure))
 			return sources
-	
-	def season_fixer(self, season):
-		if '1' in season:
-			return 'الأول'
-		elif '2' in season:
-			return 'الثانى'
-		elif '3' in season:
-			return 'الثالث'
-		elif '4' in season:
-			return 'الرابع'
-		elif '5' in season:
-			return 'الخامس'
-		elif '6' in season:
-			return 'السادس'
-		elif '7' in season:
-			return 'السابع'
-		elif '8' in season:
-			return 'الثامن'
-		elif '9' in season:
-			return 'التاسع'
-		elif '10' in season:
-			return 'العاشر'
-		elif '11' in season:
-			return 'الحادي-عشر'
-		elif '12' in season:
-			return 'الثاني-عشر'
-		elif '13' in season:
-			return 'الثالث-عشر'
-		elif '14' in season:
-			return 'الرابع-عشر'
-		elif '15' in season:
-			return 'الخامس-عشر'			
-		else:
-			return 'الأخيرة'
 
-	def resolve(self, url):	
-		return url
+    def resolve(self, url):	
+            return url
